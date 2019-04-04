009431a9716f1961cd8ef21bbfd21dcfa99bec3c
Mattia Succurro <matty2000.succ@gmail.com>
2019-04-04 17:44:14 +0200

    Adding data retrieving from forms

--- s.py
+++ s.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, abort, redirect, render_template, url_for
+from flask import (
+    Blueprint, abort, flash, redirect, render_template, request, url_for)
 import db
 import re
+import datetime
 
 bp = Blueprint('s', __name__, url_prefix='/s')
 
@@ -11,6 +13,19 @@ def canonical_person_slug(id):
     p = db.S_Person[id]
     return slugify(p.first_name) + '-' + slugify(p.last_name)
 
+def person_from_form():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    birthday_y = request.form.get('birthday_y')
+    birthday_m = request.form.get('birthday_m')
+    birthday_d = request.form.get('birthday_d')
+    try:
+        birthday = datetime.date(int(birthday_y), int(birthday_m), int(birthday_d))
+    except (TypeError, ValueError):
+        birthday = None
+    return dict(first_name=first_name, last_name=last_name,
+                birthday=birthday)
+
 @bp.route('/<slug:slug>-<int:id>/')
 @bp.route('/<int:id>/')
 def detail(id, slug=None):
@@ -20,7 +35,7 @@ def detail(id, slug=None):
             return redirect(url_for('.detail', id=id, slug=canonical_slug))
     except db.S_Person.DoesNotExist:
         abort(404)
-    return render_template('detail.html')
+    return render_template('detail.html', p=db.S_Person[id])
 
 @bp.route('/<slug:slug>-<int:id>/edit')
 @bp.route('/<int:id>/edit')
@@ -31,4 +46,17 @@ def detail_edit(id, slug=None):
             return redirect(url_for('.detail_edit', id=id, slug=canonical_slug))
     except db.S_Person.DoesNotExist:
         abort(404)
-    return render_template('detail.html')
+    if request.method == 'POST':
+        pj = person_from_form()
+        db.S_Person.update(**pj).where(S_Person.id == id).execute()
+        flash('Successfully updated')
+    return render_template('detail-edit.html', p=db.S_Person[id])
+
+@bp.route('/new/')
+def new():
+    if request.method == 'POST':
+        pj = person_from_form()
+        p = db.S_Person.create(**pj)
+        flash('Successfully created')
+        return redirect(canonical_person_slug(p.id))
+    return render_template('detail-edit.html', p=None)
